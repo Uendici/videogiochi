167c17cb78b9916f3e4ee1070574a2503a1015ce
Uendici <apesimone06@gmail.com>
2024-10-07 14:12:05 +0000

    aggiunto eroe e mappa

--- rouguelike-studio/dungeon.cpp
+++ rouguelike-studio/dungeon.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
+constexpr auto W = 10;
+constexpr auto H = 10;
 
+using namespace std;
+
+char map[W][H];
+
+void init_map(){
+
+    for(auto i = 0; i < H; i++){
+        for(auto j = 0; j < W; j++){
+            map[i][j] = '.';
+        }
+        
+    }
+
+}
+void print_entity(int x, int y, char glyph){
+    map[x][y] = glyph;
+}
 int main(){
-    std:: cout <<"hello world!\n";
+
+    
+    char hero_glyph = '@';
+    int hero_x = 5,hero_y = 7;
+    cout << hero_glyph << " ti saluta\n";
+
+    //int map
+    init_map();
+
+    //posizionamento eroe
+    print_entity(hero_x,hero_y,hero_glyph);
+    //out mappa
+    for(auto i = 0; i < H; i++){
+        for(auto j = 0; j < W; j++){
+            cout << map[i][j];
+        }
+        cout << endl;
+    }
+    return 0;
 }
