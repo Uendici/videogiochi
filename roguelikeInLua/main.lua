23f860d7a19dcd44fd2ebb0d23260a0a0ba0f3f5
Uendici <apesimone06@gmail.com>
2024-12-05 00:43:32 +0100

    Update main.lua

--- roguelikeInLua/main.lua
+++ roguelikeInLua/main.lua
@@ -1,10 +1,15 @@
-
-
--- main.lua
 local character = require("character")  -- Carica il modulo del personaggio
+local tree_asset = require("tree_asset") -- Carica il modulo dell'albero
+local enemy = require("enemy") -- Carica il modulo del nemico
+-- Variabili per la telecamera
+local camera = {x = 0, y = 0}
 
 function love.update(dt)
-    character.update(dt)  -- Chiamata alla funzione di movimento del personaggio
+    character.update(dt, tree_asset.trees)  -- Chiamata alla funzione di movimento del personaggio
+
+    -- Aggiorna la posizione della telecamera per seguire il personaggio
+    camera.x = character.x - love.graphics.getWidth() / 2
+    camera.y = character.y - love.graphics.getHeight() / 2
 end
 
 function love.draw()
@@ -13,5 +18,17 @@ function love.draw()
     love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())  -- Disegna uno sfondo che copre tutto lo schermo
 
     love.graphics.setColor(1, 1, 1)  -- Imposta il colore del disegno su bianco (default)
-    character.draw()  -- Disegna il personaggio
+
+    -- Applica la trasformazione della telecamera
+    love.graphics.push()
+    love.graphics.translate(-camera.x, -camera.y)
+
+    -- Disegna gli alberi
+    tree_asset.draw()
+
+    -- Disegna il personaggio
+    character.draw()
+
+    -- Rimuove la trasformazione 
+    love.graphics.pop()
 end
